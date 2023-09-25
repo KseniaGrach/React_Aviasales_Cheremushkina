334ae6fd57e9567ef63be032edc07091472f8fe3
Oksana Cheremushkina <gracksenia@gmail.com>
2023-09-25 23:32:54 +0300

    Настроен Eslint и Prettier

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { DevSupport } from '@react-buddy/ide-toolbox';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ComponentPreviews, useInitial } from './dev';
@@ -9,13 +10,10 @@ import { ComponentPreviews, useInitial } from './dev';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <DevSupport
-      ComponentPreviews={ComponentPreviews}
-      useInitialHook={useInitial}
-    >
+    <DevSupport ComponentPreviews={ComponentPreviews} useInitialHook={useInitial}>
       <App />
     </DevSupport>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
